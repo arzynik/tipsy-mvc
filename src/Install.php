1308c70676e6a72f98cda742096ff287cf43082d
Devin Smith <github@arzynik.com>
2015-12-04 13:22:15 -0800

    updated install script

--- src/Install.php
+++ src/Install.php
@@ -4,18 +4,32 @@ namespace Tipsy\MVC;
 
 class Install {
 	public static function run($event = null) {
-		// create directories if they do not exist
+		// copy files if they do not exist
 
+		$src = realpath(__DIR__.'/../boilerplate');
+		$dst = realpath(__DIR__.'/../../../../');
 
-		$path = realpath(__DIR__.'/../../../../');
-
-		$objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
-		foreach($objects as $name => $object){
-			echo "$name\n";
-			echo $object->getPathname()."\n";
+		if (!$src) {
+			throw new \Exception('Could not find tipsy-mvc/boilerplate');
+		}
+		if (!$dst) {
+			throw new \Exception('Lost in tipsy-mvc/boilerplate');
 		}
 
+		$objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($src), \RecursiveIteratorIterator::SELF_FIRST);
+		foreach($objects as $name => $object){
+			if (substr($object->getFilename(), 0, 1) == '.') {
+				continue;
+			}
+			$file = str_replace($src, '', $object->getPathname());
+
+			if (!file_exists($dst.$file)) {
+				if ($object->isDir()) {
+					mkdir($dst.$file);
+				} else {
+					copy($object->getPathname(), $dst.$file);
+				}
+			}
+		}
 	}
 }
-
-Install::run();
